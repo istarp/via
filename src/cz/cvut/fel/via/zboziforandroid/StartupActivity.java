4194935f13eb0f44ae835c72c22adbd15d0e9da4
Jan Novacek <jan.novacek@centrum.cz>
2012-11-12 11:18:03 +0100

    Auto-complete updated

--- src/cz/cvut/fel/via/zboziforandroid/StartupActivity.java
+++ src/cz/cvut/fel/via/zboziforandroid/StartupActivity.java
@@ -4,6 +4,8 @@ import cz.cvut.fel.via.zboziforandroid.model.QuerryDatabase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,7 +15,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class StartupActivity extends FragmentActivity{	
+public class StartupActivity extends FragmentActivity implements TextWatcher {	
+	
+	private AutoCompleteTextView searchedString;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,9 @@ public class StartupActivity extends FragmentActivity{
         setContentView(R.layout.activity_startup);
         
         final Button searchButton = (Button) findViewById(R.id.searchButton);
-        final AutoCompleteTextView searchedString = (AutoCompleteTextView) findViewById(R.id.searchString);        
+        searchedString = (AutoCompleteTextView) findViewById(R.id.searchString);        
         
+        searchedString.addTextChangedListener(this);
         searchedString.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, QuerryDatabase.QUERIES));
         
         searchedString.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -51,5 +56,22 @@ public class StartupActivity extends FragmentActivity{
         Intent listIntent = new Intent(this, ProductListActivity.class);        
         startActivity(listIntent);    	
     }
+
+	@Override
+	public void afterTextChanged(Editable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		searchedString.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, QuerryDatabase.QUERIES));		
+	}
 	
 }
