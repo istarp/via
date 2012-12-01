acaa16c2753f4de13c4631944788891db197497b
Jan Novacek <jan.novacek@centrum.cz>
2012-12-01 01:57:46 +0100

    Missing files added

---
+++ src/cz/cvut/fel/via/zboziforandroid/model/Const.java
@@ -0,0 +1,9 @@
+package cz.cvut.fel.via.zboziforandroid.model;
+
+public class Const {
+
+	// must corresponded with arrays in res string...
+	public static String[] PRODUCT_CRITERIONS = {"relevance", "price", "name"};
+	public static String[] PRODUCT_DIRECTION = {"asc", "desc"};
+	
+}
