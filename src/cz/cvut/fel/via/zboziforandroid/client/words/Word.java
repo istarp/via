caaefab29098f51a35f472d86e028af6b709a450
Petr Svobodnik <svobodnik.petr@gmail.com>
2012-12-01 10:12:26 +0100

    prepare for merge

---
+++ src/cz/cvut/fel/via/zboziforandroid/client/words/Word.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.via.zboziforandroid.client.words;
+
+public class Word {
+    private int numberOfSearch;
+    private String word;
+    private int id;
+
+    @Override
+    public String toString() {
+        return "Word{" + "numberOfSearch=" + numberOfSearch + ", word=" + word + ", id=" + id + '}';
+    }
+    
+}
